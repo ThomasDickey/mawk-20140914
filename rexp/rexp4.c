6c0741a7017a6efd8754b70c01df8a1f6e6f4ed2
Thomas E. Dickey <dickey@invisible-island.net>
2009-07-09 23:23:11 +0000

    apply fix from Debian #355966

--- rexp/rexp4.c
+++ rexp/rexp4.c
@@ -5,7 +5,7 @@ is_string_split(p, lenp)
    register STATE *p ;
    unsigned *lenp ;
 {
-   if (p[0].type == M_STR && p[1].type == M_ACCEPT)
+   if (p != 0 && (p[0].type == M_STR && p[1].type == M_ACCEPT))
    {
       *lenp = p->len ;
       return p->data.str ;
