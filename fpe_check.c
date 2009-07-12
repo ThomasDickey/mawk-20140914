a0145324254bbea2c67f9f98f599cb28c37ff306
Thomas E. Dickey <dickey@invisible-island.net>
2009-07-12 21:18:40 +0000

    some ANSIfication (two seem to rely on further testing), and indent'd

--- fpe_check.c
+++ fpe_check.c
@@ -2,7 +2,9 @@
    floating point exception handling does.
 */
 
-/* @Log: fpe_check.c,v @
+/*
+ * $MawkId: fpe_check.c,v 1.3 2009/07/12 21:18:40 tom Exp $
+ * @Log: fpe_check.c,v @
  * Revision 1.7  1996/08/30 00:07:14  mike
  * Modifications to the test and implementation of the bug fix for
  * solaris overflow in strtod.
@@ -35,101 +37,89 @@
 _LIB_VERSION_TYPE _LIB_VERSION = _IEEE_;
 #endif
 
-void message(s)
-   char *s ;
+static void
+message(char *s)
 {
-   printf("\t%s\n", s) ;
+    printf("\t%s\n", s);
 }
 
-jmp_buf jbuff ;
-int may_be_safe_to_look_at_why = 0 ;
-int why_v ;
-int checking_for_strtod_ovf_bug = 0 ;
+jmp_buf jbuff;
+int may_be_safe_to_look_at_why = 0;
+int why_v;
+int checking_for_strtod_ovf_bug = 0;
 
-RETSIGTYPE fpe_catch() ;
-int is_nan() ;
-void check_strtod_ovf() ;
-double strtod() ;
+static RETSIGTYPE fpe_catch();
+static int is_nan(double);
+static void check_strtod_ovf(void);
+extern double strtod();
 
-double
-div_by(x,y)
-   double x ;
-   double y ;
+static double
+div_by(double x, double y)
 {
-   return x/y ;
+    return x / y;
 }
 
-double overflow(x)
-   double x ;
+static double
+overflow(double x)
 {
-   double y ;
+    double y;
 
-   do
-   {
-      y = x ;
-      x *= x ;
-   } while( y != x ) ;
-   return x ;
+    do {
+	y = x;
+	x *= x;
+    } while (y != x);
+    return x;
 }
 
-   
-void check_fpe_traps()
+static void
+check_fpe_traps(void)
 {
-   int traps = 0 ;
+    int traps = 0;
 
-   if (setjmp(jbuff) == 0)
-   {
-      div_by(44.0, 0.0) ;
-      message("division by zero does not generate an exception") ;
-   }
-   else
-   {
-      traps = 1 ;
-      message("division by zero generates an exception") ;
-      signal(SIGFPE, fpe_catch) ; /* set again if sysV */
-   }
+    if (setjmp(jbuff) == 0) {
+	div_by(44.0, 0.0);
+	message("division by zero does not generate an exception");
+    } else {
+	traps = 1;
+	message("division by zero generates an exception");
+	signal(SIGFPE, fpe_catch);	/* set again if sysV */
+    }
 
-   if ( setjmp(jbuff) == 0 )
-   {
-      overflow(1000.0) ;
-      message("overflow does not generate an exception") ;
-   }
-   else
-   {
-      traps |= 2 ;
-      message("overflow generates an exception") ;
-      signal(SIGFPE, fpe_catch) ; 
-   }
+    if (setjmp(jbuff) == 0) {
+	overflow(1000.0);
+	message("overflow does not generate an exception");
+    } else {
+	traps |= 2;
+	message("overflow generates an exception");
+	signal(SIGFPE, fpe_catch);
+    }
 
-   if ( traps == 0 )
-   {
-      double maybe_nan = log(-8.0) ;
+    if (traps == 0) {
+	double maybe_nan = log(-8.0);
 
-      if (is_nan(maybe_nan))
-      {
-	 message("math library supports ieee754") ;
-      }
-      else
-      {
-	 traps |= 4 ;
-	 message("math library does not support ieee754") ;
-      }
-   }
+	if (is_nan(maybe_nan)) {
+	    message("math library supports ieee754");
+	} else {
+	    traps |= 4;
+	    message("math library does not support ieee754");
+	}
+    }
 
-   exit(traps) ;
+    exit(traps);
 }
 
-int is_nan(d)
-   double d ;
+static int
+is_nan(double d)
 {
-   char command[128] ;
+    char command[128];
 
-   if (!(d==d))  return 1 ;
+    if (!(d == d))
+	return 1;
 
-   /* on some systems with an ieee754 bug, we need to make another check */
-   sprintf(command, 
-	   "echo '%f' | egrep '[nN][aA][nN]|\\?' >/dev/null", d) ; 
-   return system(command)==0 ;
+    /* on some systems with an ieee754 bug, we need to make another check */
+    sprintf(command,
+	    "echo '%f' | egrep '[nN][aA][nN]|\\?' >/dev/null", d);
+    return system(command) == 0;
 }
 
 /*
@@ -137,70 +127,70 @@ Only get here if we think we have Berkeley type signals so we can
 look at a second argument to fpe_catch() to get the reason for
 an exception
 */
-void
-get_fpe_codes()  
+static void
+get_fpe_codes(void)
 {
-   int divz ;
-   int ovf ;
+    int divz = 0;
+    int ovf = 0;
 
-   may_be_safe_to_look_at_why = 1 ;
+    may_be_safe_to_look_at_why = 1;
 
-   if( setjmp(jbuff) == 0 ) div_by(1000.0, 0.0) ;
-   else  
-   {
-      divz = why_v ;
-      signal(SIGFPE, fpe_catch) ;
-   }
+    if (setjmp(jbuff) == 0)
+	div_by(1000.0, 0.0);
+    else {
+	divz = why_v;
+	signal(SIGFPE, fpe_catch);
+    }
 
-   if( setjmp(jbuff) == 0 ) overflow(1000.0) ;
-   else  
-   {
-      ovf = why_v ;
-      signal(SIGFPE, fpe_catch) ;
-   }
+    if (setjmp(jbuff) == 0)
+	overflow(1000.0);
+    else {
+	ovf = why_v;
+	signal(SIGFPE, fpe_catch);
+    }
 
-
-   /* make some guesses if sane values */
-   if ( divz>0 && ovf>0 && divz != ovf )
-   {
-      printf("X FPE_ZERODIVIDE %d\n", divz) ;
-      printf("X FPE_OVERFLOW %d\n", ovf) ;
-      exit(0) ;
-   }
-   else exit(1) ;
+    /* make some guesses if sane values */
+    if (divz > 0 && ovf > 0 && divz != ovf) {
+	printf("X FPE_ZERODIVIDE %d\n", divz);
+	printf("X FPE_OVERFLOW %d\n", ovf);
+	exit(0);
+    } else
+	exit(1);
 }
 
 int
-main(argc)
-   int argc ;
+main(int argc, char *argv[])
 {
 
-   signal(SIGFPE, fpe_catch) ;
-   switch(argc) {
-      case 1 : 
-	 check_fpe_traps() ;
-	 break ;
-      case 2 : 
-	 get_fpe_codes() ;
-	 break ;
-      default: 
-	 check_strtod_ovf() ;
-	 break ;
-   }
-   /* not reached */
-   return 0 ;
-} 
+    signal(SIGFPE, fpe_catch);
+    switch (argc) {
+    case 1:
+	check_fpe_traps();
+	break;
+    case 2:
+	get_fpe_codes();
+	break;
+    default:
+	check_strtod_ovf();
+	break;
+    }
+    /* not reached */
+    return 0;
+}
 
 /* put this down here in attempt to defeat ambitious compiler that
    may have seen a prototype without 2nd argument */
-   
-RETSIGTYPE fpe_catch(signal, why)
-   int signal ;
-   int why ;
+
+static RETSIGTYPE
+fpe_catch(sig, why)
+     int sig;
+     int why;
 {
-   if (checking_for_strtod_ovf_bug) exit(1) ;
-   if ( may_be_safe_to_look_at_why ) why_v = why ;
-   longjmp(jbuff,1) ;
+    if (checking_for_strtod_ovf_bug)
+	exit(1);
+    if (may_be_safe_to_look_at_why)
+	why_v = why;
+    longjmp(jbuff, 1);
 }
 
 char longstr[] =
@@ -243,22 +233,20 @@ char longstr[] =
 1234567890\
 1234567890\
 1234567890\
-1234567890" ;
+1234567890";
 
 #ifdef  USE_IEEEFP_H
 #include <ieeefp.h>
 #endif
 
-void
-check_strtod_ovf()
+static void
+check_strtod_ovf(void)
 {
-    double x ; 
-
 #ifdef USE_IEEEFP_H
-    fpsetmask(fpgetmask()|FP_X_OFL|FP_X_DZ) ;
+    fpsetmask(fpgetmask() | FP_X_OFL | FP_X_DZ);
 #endif
 
-    checking_for_strtod_ovf_bug = 1 ;
-    strtod(longstr,(char**)0) ;
-    exit(0) ;
+    checking_for_strtod_ovf_bug = 1;
+    strtod(longstr, (char **) 0);
+    exit(0);
 }
