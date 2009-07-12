37e5979f4daa35a1e4a990c92d43b2d8f88870c9
Thomas E. Dickey <dickey@invisible-island.net>
2009-07-12 11:11:23 +0000

    fix gcc warning for cell_destroy()

--- mawk.h
+++ mawk.h
@@ -10,7 +10,7 @@ the GNU General Public License, version 2, 1991.
 ********************************************/
 
 /*
- * $MawkId: mawk.h,v 1.5 2009/07/12 10:51:34 tom Exp $
+ * $MawkId: mawk.h,v 1.6 2009/07/12 11:11:23 tom Exp $
  * @Log: mawk.h,v @
  * Revision 1.10  1996/08/25 19:31:04  mike
  * Added work-around for solaris strtod overflow bug.
@@ -119,9 +119,12 @@ extern  unsigned rt_nr , rt_fnr ; /* ditto */
 #define cell_destroy(cp)  DB_cell_destroy(cp)
 #else
 
-#define cell_destroy(cp)   if ( (cp)->type >= C_STRING &&\
-                           -- string(cp)->ref_cnt == 0 )\
-                        zfree(string(cp),string(cp)->len+STRING_OH);else
+#define cell_destroy(cp) \
+	do { \
+	    if ( (cp)->type >= C_STRING && \
+	       -- string(cp)->ref_cnt == 0 ) \
+	    zfree(string(cp),string(cp)->len+STRING_OH); \
+	} while (0)
 #endif
 
 /*  prototypes  */
