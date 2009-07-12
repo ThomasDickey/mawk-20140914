f63850f2f6273f546f78cffde49eeac92316914a
Thomas E. Dickey <dickey@invisible-island.net>
2009-07-12 18:26:22 +0000

    fix missing prototype

--- rexp/rexp4.c
+++ rexp/rexp4.c
@@ -1,11 +1,15 @@
 /*
- * $MawkId: rexp4.c,v 1.3 2009/07/12 17:56:57 tom Exp $
+ * $MawkId: rexp4.c,v 1.4 2009/07/12 18:26:22 tom Exp $
  */
+#include "mawk.h"
 #include "rexp.h"
+#include "field.h"
 
 char *
-is_string_split(STATE * p, unsigned *lenp)
+is_string_split(PTR q, unsigned *lenp)
 {
+    STATE *p = (STATE *) q;
+
     if (p != 0 && (p[0].type == M_STR && p[1].type == M_ACCEPT)) {
 	*lenp = p->len;
 	return p->data.str;
