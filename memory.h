1399e7f108f849c3daa65d0869d692c7e9b62cb7
Thomas E. Dickey <dickey@invisible-island.net>
2009-07-23 22:02:50 +0000

    indent'd

--- memory.h
+++ memory.h
@@ -9,8 +9,9 @@ Mawk is distributed without warranty under the terms of
 the GNU General Public License, version 2, 1991.
 ********************************************/
 
-
-/* @Log: memory.h,v @
+/*
+ * $MawkId: memory.h,v 1.3 2009/07/23 22:02:50 tom Exp $
+ * @Log: memory.h,v @
  * Revision 1.1.1.1  1993/07/03  18:58:17  mike
  * move source to cvs
  *
@@ -29,11 +30,11 @@ the GNU General Public License, version 2, 1991.
 
 #include "zmalloc.h"
 
-STRING *new_STRING(char*);
+STRING *new_STRING(char *);
 STRING *new_STRING0(unsigned);
 
 #ifdef   DEBUG
-void  DB_free_STRING(STRING *);
+void DB_free_STRING(STRING *);
 
 #define  free_STRING(s)  DB_free_STRING(s)
 
@@ -46,4 +47,4 @@ void  DB_free_STRING(STRING *);
 	    } while (0)
 #endif
 
-#endif   /* MEMORY_H */
+#endif /* MEMORY_H */
