8bd7b0af820da86bf815597675a7e2f4ec73be2b
Thomas E. Dickey <dickey@invisible-island.net>
2009-07-28 23:30:28 +0000

    sigh.

--- patchlev.h
+++ patchlev.h
@@ -1,7 +1,7 @@
 /*
- * $MawkId: patchlev.h,v 1.5 2009/07/27 23:21:24 tom Exp $
+ * $MawkId: patchlev.h,v 1.6 2009/07/28 23:30:28 tom Exp $
  */
 #define  PATCHLEVEL	3
 #define  PATCH_STRING	".3"
-#define  DATE_STRING    "20090727"
+#define  DATE_STRING    "20090728"
 #define  MAWK_ID	"@(#)mawk 1.3.3"
