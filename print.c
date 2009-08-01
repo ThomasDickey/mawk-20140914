5c5584da13419a10a37a82036be74b9d420dec79
Thomas E. Dickey <dickey@invisible-island.net>
2009-08-01 00:53:15 +0000

    first draft (still debugging) of a rewrite of the handling of s-format in do_printf().  The rewrite will support optional leading 0's for a string. It also will pass nulls ("binary" data).

--- print.c
+++ print.c
@@ -10,7 +10,7 @@ the GNU General Public License, version 2, 1991.
 ********************************************/
 
 /*
- * $MawkId: print.c,v 1.8 2009/07/27 15:32:48 tom Exp $
+ * $MawkId: print.c,v 1.9 2009/08/01 00:53:15 tom Exp $
  * @Log: print.c,v @
  * Revision 1.7  1996/09/18 01:04:36  mike
  * Check ferror() after print and printf.
@@ -201,11 +201,242 @@ bad_conversion(int cnt, const char *who, const char *format)
 	     cnt, who, format);
 }
 
-/* the contents of format are preserved,
-   caller does CELL cleanup
+typedef enum {
+    sfmtSPACE = 1,
+    sfmtMINUS = 2,
+    sfmtZERO = 4,
+    sfmtWIDTH = 8,
+    sfmtPREC = 16
+} enumSFMT;
 
-   This routine does both printf and sprintf (if fp==0)
-*/
+/*
+ * nawk pads leading 0's if it sees a format such as "%06s".
+ * C's printf/sprintf do not do that.
+ * So we decode the format and interpret it according to nawk.
+ */
+static int
+make_sfmt(const char *format,
+	  int *fill_in,
+	  int *width,
+	  int *prec,
+	  int *flags)
+{
+    int ch;
+    int parts = 0;
+    int digits = 0;
+    int value = 0;
+    int used = 0;
+    int success = 1;
+    int ok_flag = 1;
+
+    *width = 0;
+    *prec = 0;
+    *flags = 0;
+
+    ++format;
+    while (*format != '\0' && ok_flag) {
+	switch (*format++) {
+	case ' ':
+	    *flags |= sfmtSPACE;
+	    break;
+	case '-':
+	    *flags |= sfmtMINUS;
+	    break;
+	case '0':
+	    *flags |= sfmtZERO;
+	    break;
+	default:
+	    ok_flag = 0;
+	    --format;
+	    break;
+	}
+    }
+
+    while (*format != '\0' && success) {
+	switch (ch = *format++) {
+	case '0':
+	case '1':
+	case '2':
+	case '3':
+	case '4':
+	case '5':
+	case '6':
+	case '7':
+	case '8':
+	case '9':
+	    if (digits < 0) {
+		success = 0;
+	    } else if (digits++ == 0) {
+		value = (ch - '0');
+	    } else {
+		value = (10 * value) + (ch - '0');
+	    }
+	    break;
+	case '.':
+	    if (parts) {
+		success = 0;
+	    } else {
+		if (digits) {
+		    *width = value;
+		    *flags |= sfmtWIDTH;
+		    digits = 0;
+		    value = 0;
+		}
+		parts = 1;
+	    }
+	    break;
+	case '*':
+	    if (digits == 0) {
+		value = fill_in[used++];
+		digits = -1;
+	    } else {
+		success = 0;
+	    }
+	    break;
+	case 's':
+	    if (digits) {
+		if (parts) {
+		    *prec = value;
+		    *flags |= sfmtPREC;
+		} else {
+		    *width = value;
+		    *flags |= sfmtWIDTH;
+		}
+	    }
+	    if (*format != '\0')
+		success = 0;
+	    break;
+	default:
+	    success = 0;
+	    break;
+	}
+    }
+
+    return success;
+}
+
+#define SprintfOverflow(buffer, need) \
+    if (buffer + need + 1 >= sprintf_limit) { \
+	    rt_overflow("sprintf buffer", \
+			(unsigned) (sprintf_limit - sprintf_buff)); \
+    }
+
+static char *
+SprintfChar(char *buffer, int ch)
+{
+    SprintfOverflow(buffer, 1);
+    *buffer++ = (char) ch;
+    return buffer;
+}
+
+static char *
+SprintfFill(char *buffer, int ch, int fill)
+{
+    SprintfOverflow(buffer, fill);
+    memset(buffer, ch, fill);
+    return buffer + fill;
+}
+
+static char *
+SprintfBlock(char *buffer, char *source, int length)
+{
+    SprintfOverflow(buffer, length);
+    memcpy(buffer, source, length);
+    return buffer + length;
+}
+
+/*
+ * Write the s-format text.
+ */
+static PTR
+puts_sfmt(PTR target, FILE *fp, CELL * source, int width, int prec, int flags)
+{
+    char *src_str = string(source)->str;
+    int src_len = string(source)->len;
+
+    if (width < 0) {
+	width = -width;
+	flags |= sfmtMINUS;
+    }
+
+    if (fp != 0) {
+
+	if (flags & sfmtSPACE) {
+	    if (src_len == 0)
+		fputc(' ', fp);
+	}
+	if (flags & sfmtPREC) {
+	    if (src_len > prec)
+		src_len = prec;
+	}
+	if (!(flags & sfmtWIDTH)) {
+	    if (flags & sfmtPREC) {
+		width = prec;
+	    } else {
+		width = src_len;
+	    }
+	}
+	if ((flags & sfmtZERO) && !(flags & sfmtMINUS)) {
+	    while (src_len < width) {
+		fputc('0', fp);
+		--width;
+	    }
+	}
+	if (!(flags & sfmtMINUS)) {
+	    while (src_len < width) {
+		fputc(' ', fp);
+		--width;
+	    }
+	}
+	fwrite(src_str, sizeof(char), src_len, fp);
+	while (src_len < width) {
+	    fputc(' ', fp);
+	    --width;
+	}
+    } else {
+	char *buffer = (char *) target;		/* points into sprintf_buff */
+
+	if (flags & sfmtSPACE) {
+	    if (src_len == 0) {
+		buffer = SprintfChar(buffer, ' ');
+	    }
+	}
+	if (flags & sfmtPREC) {
+	    if (src_len > prec)
+		src_len = prec;
+	}
+	if (!(flags & sfmtWIDTH)) {
+	    if (flags & sfmtPREC) {
+		width = prec;
+	    } else {
+		width = src_len;
+	    }
+	}
+	if ((flags & sfmtZERO) && !(flags & sfmtMINUS)) {
+	    buffer = SprintfFill(buffer, '0', width - src_len);
+	    width = src_len;
+	}
+	if (!(flags & sfmtMINUS)) {
+	    if (src_len < width) {
+		buffer = SprintfFill(buffer, ' ', width - src_len);
+		width = src_len;
+	    }
+	}
+	buffer = SprintfBlock(buffer, src_str, src_len);
+	if (src_len < width) {
+	    buffer = SprintfFill(buffer, ' ', width - src_len);
+	}
+	target = buffer;
+    }
+    return target;
+}
+
+/* 
+ * Note: caller must do CELL cleanup.
+ * The format parameter is modified, but restored.
+ *
+ * This routine does both printf and sprintf (if fp==0)
+ */
 static STRING *
 do_printf(
 	     FILE *fp,
@@ -223,6 +454,7 @@ do_printf(
     int ast[2];
     UInt Uval = 0;
     Int Ival = 0;
+    int sfmt_width, sfmt_prec, sfmt_flags, s_format;
     int num_conversion = 0;	/* for error messages */
     const char *who;		/*ditto */
     int pf_type = 0;		/* conversion type */
@@ -232,8 +464,7 @@ do_printf(
     char xbuff[256];		/* splice in l qualifier here */
 #endif
 
-    if (fp == (FILE *) 0)	/* doing sprintf */
-    {
+    if (fp == (FILE *) 0) {	/* doing sprintf */
 	target = sprintf_buff;
 	printer = (PRINTER) sprintf;
 	who = "sprintf";
@@ -244,8 +475,7 @@ do_printf(
     }
 
     while (1) {
-	if (fp)			/* printf */
-	{
+	if (fp) {		/* printf */
 	    while (*q != '%') {
 		if (*q == 0) {
 		    if (ferror(fp))
@@ -258,7 +488,7 @@ do_printf(
 		}
 	    }
 	} else {		/* sprintf */
-	    while (*q != '%')
+	    while (*q != '%') {
 		if (*q == 0) {
 		    if (target > sprintf_limit)		/* damaged */
 		    {
@@ -266,22 +496,19 @@ do_printf(
 			rt_overflow("sprintf buffer",
 				    (unsigned) (sprintf_limit - sprintf_buff));
 		    } else {	/* really done */
-			STRING *retval;
-			unsigned len = (unsigned) (target - sprintf_buff);
-
-			retval = new_STRING0(len);
-			memcpy(retval->str, sprintf_buff, len);
-			return retval;
+			return new_STRING1(sprintf_buff,
+					   (unsigned) (target - sprintf_buff));
 		    }
-		} else
+		} else {
 		    *target++ = *q++;
+		}
+	    }
 	}
 
 	/* *q == '%' */
 	num_conversion++;
 
-	if (*++q == '%')	/* %% */
-	{
+	if (*++q == '%') {	/* %% */
 	    if (fp)
 		putc(*q, fp);
 	    else
@@ -311,8 +538,7 @@ do_printf(
 		q++;
 	/* width is done */
 
-	if (*q == '.')		/* have precision */
-	{
+	if (*q == '.') {	/* have precision */
 	    q++;
 	    if (*q == '*') {
 		if (cp->type != C_DOUBLE)
@@ -320,9 +546,10 @@ do_printf(
 		ast[ast_cnt++] = d_to_i(cp++->dval);
 		argcnt--;
 		q++;
-	    } else
+	    } else {
 		while (scan_code[*(unsigned char *) q] == SC_DIGIT)
 		    q++;
+	    }
 	}
 
 	if (argcnt <= 0)
@@ -441,6 +668,7 @@ do_printf(
 #endif
 
 	/* ready to call printf() */
+	s_format = 0;
 	switch (AST(ast_cnt, pf_type)) {
 	case AST(0, PF_C):
 	    (*printer) ((PTR) target, p, (int) Ival);
@@ -455,15 +683,21 @@ do_printf(
 	    break;
 
 	case AST(0, PF_S):
-	    (*printer) ((PTR) target, p, string(cp)->str);
+	    s_format = 1;
+	    make_sfmt(p, ast, &sfmt_width, &sfmt_prec, &sfmt_flags);
+	    target = puts_sfmt(target, fp, cp, sfmt_width, sfmt_prec, sfmt_flags);
 	    break;
 
 	case AST(1, PF_S):
-	    (*printer) ((PTR) target, p, ast[0], string(cp)->str);
+	    s_format = 1;
+	    make_sfmt(p, ast, &sfmt_width, &sfmt_prec, &sfmt_flags);
+	    target = puts_sfmt(target, fp, cp, sfmt_width, sfmt_prec, sfmt_flags);
 	    break;
 
 	case AST(2, PF_S):
-	    (*printer) ((PTR) target, p, ast[0], ast[1], string(cp)->str);
+	    s_format = 1;
+	    make_sfmt(p, ast, &sfmt_width, &sfmt_prec, &sfmt_flags);
+	    target = puts_sfmt(target, fp, cp, sfmt_width, sfmt_prec, sfmt_flags);
 	    break;
 
 #ifdef	SHORT_INTS
@@ -509,9 +743,10 @@ do_printf(
 	    (*printer) ((PTR) target, p, ast[0], ast[1], cp->dval);
 	    break;
 	}
-	if (fp == (FILE *) 0)
+	if (fp == (FILE *) 0 && !s_format) {
 	    while (*target)
 		target++;
+	}
 	*q = save;
 	argcnt--;
 	cp++;
