74c69dd36e75cf1c0ad28999d04eca4199b77565
Thomas E. Dickey <dickey@invisible-island.net>
2009-09-21 00:17:49 +0000

    mark

--- patchlev.h
+++ patchlev.h
@@ -1,6 +1,6 @@
 /*
- * $MawkId: patchlev.h,v 1.10 2009/09/14 09:32:29 tom Exp $
+ * $MawkId: patchlev.h,v 1.11 2009/09/21 00:17:49 tom Exp $
  */
 #define  PATCHLEVEL	3
 #define  PATCH_STRING	".3"
-#define  DATE_STRING    "20090914"
+#define  DATE_STRING    "20090920"
