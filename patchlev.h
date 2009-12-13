294bdacdaee0dfaf7dbd2bc76d4b2ab1a2766613
Thomas E. Dickey <dickey@invisible-island.net>
2009-12-13 21:33:17 +0000

    snapshot for today (not urgent)

--- patchlev.h
+++ patchlev.h
@@ -1,6 +1,6 @@
 /*
- * $MawkId: patchlev.h,v 1.11 2009/09/21 00:17:49 tom Exp $
+ * $MawkId: patchlev.h,v 1.12 2009/12/13 21:33:17 tom Exp $
  */
 #define  PATCHLEVEL	3
 #define  PATCH_STRING	".3"
-#define  DATE_STRING    "20090920"
+#define  DATE_STRING    "20091213"
