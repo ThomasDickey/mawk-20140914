4010fce865a45e9b6414fbdeff95cb52dd8ef376
Thomas E. Dickey <dickey@invisible-island.net>
2010-04-19 09:06:56 +0000

    bump

--- patchlev.h
+++ patchlev.h
@@ -1,6 +1,6 @@
 /*
- * $MawkId: patchlev.h,v 1.21 2010/04/19 00:02:21 tom Exp $
+ * $MawkId: patchlev.h,v 1.22 2010/04/19 09:06:56 tom Exp $
  */
 #define  PATCHLEVEL	3
 #define  PATCH_STRING	".4"
-#define  DATE_STRING    "20100418"
+#define  DATE_STRING    "20100419"
