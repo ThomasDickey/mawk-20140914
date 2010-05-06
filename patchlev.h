84649952e3aa8c1c3f9bf711e523e8e8e2898536
Thomas E. Dickey <dickey@invisible-island.net>
2010-05-06 22:22:12 +0000

    target tomorrow

--- patchlev.h
+++ patchlev.h
@@ -1,6 +1,6 @@
 /*
- * $MawkId: patchlev.h,v 1.22 2010/04/19 09:06:56 tom Exp $
+ * $MawkId: patchlev.h,v 1.23 2010/05/06 22:22:12 tom Exp $
  */
 #define  PATCHLEVEL	3
 #define  PATCH_STRING	".4"
-#define  DATE_STRING    "20100419"
+#define  DATE_STRING    "20100507"
