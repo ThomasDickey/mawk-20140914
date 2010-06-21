f9511460d8c5d34e8d784a47b98c634779a7eb6e
Thomas E. Dickey <dickey@invisible-island.net>
2010-06-21 21:42:17 +0000

    move PATCH_BASE from version.c

--- patchlev.h
+++ patchlev.h
@@ -1,6 +1,7 @@
 /*
- * $MawkId: patchlev.h,v 1.24 2010/06/19 00:52:41 tom Exp $
+ * $MawkId: patchlev.h,v 1.25 2010/06/21 21:42:17 tom Exp $
  */
-#define  PATCHLEVEL	3
+#define  PATCH_BASE	1
+#define  PATCH_LEVEL	3
 #define  PATCH_STRING	".4"
 #define  DATE_STRING    "20100618"
