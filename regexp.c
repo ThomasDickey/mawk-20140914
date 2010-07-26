3ad16ec8c5d88fec2da3eb431103148f6aae7887
Thomas E. Dickey <dickey@invisible-island.net>
2010-07-26 09:28:39 +0000

    account for leak in rexp2.c

--- regexp.c
+++ regexp.c
@@ -1,3 +1,4 @@
+/* $MawkId* */
 #ifdef LOCAL_REGEXP
 #		include "rexp.c"
 #		include "rexp0.c"
@@ -9,4 +10,21 @@
 #else
 #		include "rexp4.c"
 #		include "regexp_system.c"
+#endif
+
+#ifdef NO_LEAKS
+void
+rexp_leaks(void)
+{
+#ifdef LOCAL_REGEXP
+    if (RE_run_stack_base) {
+	free(RE_run_stack_base);
+	RE_run_stack_base = 0;
+    }
+    if (RE_pos_stack_base) {
+	free(RE_pos_stack_base);
+	RE_pos_stack_base = 0;
+    }
+#endif
+}
 #endif
