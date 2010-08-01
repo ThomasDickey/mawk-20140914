ff1770e1d79d2196feacaabd497ee8b5a9844182
Thomas E. Dickey <dickey@invisible-island.net>
2010-08-01 15:28:03 +0000

    working on no-leaks for user functions

--- code.c
+++ code.c
@@ -10,7 +10,7 @@ the GNU General Public License, version 2, 1991.
 ********************************************/
 
 /*
- * $MawkId: code.c,v 1.15 2010/07/31 21:34:38 tom Exp $
+ * $MawkId: code.c,v 1.16 2010/08/01 15:28:03 tom Exp $
  * @Log: code.c,v @
  * Revision 1.6  1995/06/18  19:42:13  mike
  * Remove some redundant declarations and add some prototypes
@@ -278,7 +278,7 @@ free_cell_data(CELL * cp)
     }
 }
 
-static void
+void
 free_codes(const char *tag, INST * base, size_t size)
 {
     INST *cdp;
@@ -301,18 +301,22 @@ free_codes(const char *tag, INST * base, size_t size)
 	case F_PUSHA:
 	case LAE_PUSHA:
 	case LAE_PUSHI:
-	case LA_PUSHA:
-	case L_PUSHA:
-	case L_PUSHI:
 	case _MATCH0:
 	case _MATCH1:
 	case _PUSHC:
 	    ++cdp;		/* skip pointer */
 	    cp = (CELL *) (cdp->ptr);
-	    TRACE(("\tparam %p type %d\n", cp, cp->type));
-	    free_cell_data(cp);
+	    if (cp != 0) {
+		TRACE(("\tparam %p type %d\n", cp, cp->type));
+		free_cell_data(cp);
+	    } else {
+		TRACE(("\tparam %p type ??\n", cp));
+	    }
 	    break;
 	case A_PUSHA:
+	case L_PUSHA:
+	case L_PUSHI:
+	case LA_PUSHA:
 	case _BUILTIN:
 	case _PRINT:
 	case _PUSHA:
