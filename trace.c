756b6621da4418cfeeee3ccf5be6fb2d6ef0de50
Thomas E. Dickey <dickey@invisible-island.net>
2012-10-30 22:58:41 +0000

    add TraceCell()

--- trace.c
+++ trace.c
@@ -10,7 +10,7 @@ the GNU General Public License, version 2, 1991.
 ********************************************/
 
 /*
- * $MawkId: trace.c,v 1.1 2012/10/30 21:44:45 tom Exp $
+ * $MawkId: trace.c,v 1.2 2012/10/30 22:58:41 tom Exp $
  */
 #include <stdarg.h>
 
@@ -32,6 +32,51 @@ Trace(const char *format,...)
     va_start(args, format);
     vfprintf(trace_fp, format, args);
     va_end(args);
+}
+
+void
+TraceCell(CELL * cp)
+{
+    TRACE(("cell %p ", cp));
+    if (cp != 0) {
+	switch ((MAWK_CELL_TYPES) cp->type) {
+	case C_NOINIT:
+	    TRACE(("is empty\n"));
+	    break;
+	case C_DOUBLE:
+	    TRACE(("double %g\n", cp->dval));
+	    break;
+	case C_MBSTRN:
+	case C_STRNUM:
+	case C_STRING:
+	    if (string(cp)->len) {
+		TRACE(("string '%.*s'\n", (int) string(cp)->len, string(cp)->str));
+	    } else {
+		TRACE(("string ''\n"));
+	    }
+	    break;
+	case C_SPACE:
+	    TRACE(("split on space\n"));
+	    break;
+	case C_SNULL:
+	    TRACE(("split on the empty string\n"));
+	    break;
+	case C_RE:
+	    TRACE(("a regular expression at %p\n", cp->ptr));
+	    break;
+	case C_REPL:
+	    TRACE(("a replacement string at %p\n", cp->ptr));
+	    break;
+	case C_REPLV:
+	    TRACE(("a vector replacement, count %d at %p\n", cp->vcnt, cp->ptr));
+	    break;
+	case NUM_CELL_TYPES:
+	    TRACE(("unknown type %d\n", cp->type));
+	    break;
+	}
+    } else {
+	TRACE(("no cell\n"));
+    }
 }
 
 #ifdef NO_LEAKS
