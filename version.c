87de7da25abdbe5035adfc88bb5f7ac400615dfe
Thomas E. Dickey <dickey@invisible-island.net>
2014-09-07 16:27:26 +0000

    show random-function names in version message also show Max_Int, as a reminder.

--- version.c
+++ version.c
@@ -11,7 +11,7 @@ the GNU General Public License, version 2, 1991.
 ********************************************/
 
 /*
- * $MawkId: version.c,v 1.18 2014/08/18 20:34:56 tom Exp $
+ * $MawkId: version.c,v 1.19 2014/09/07 16:27:26 tom Exp $
  */
 
 #include "mawk.h"
@@ -23,7 +23,8 @@ the GNU General Public License, version 2, 1991.
 Copyright 2008-2013,2014, Thomas E. Dickey\n\
 Copyright 1991-1996,2014, Michael D. Brennan\n\n"
 
-static const char fmt[] = "%-14s%10lu\n";
+#define FMT_N "%-20s%.0f\n"
+#define FMT_S "%-20s%s\n"
 
 /* print VERSION and exit */
 void
@@ -32,12 +33,27 @@ print_version(void)
     printf(VERSION_STRING, PATCH_BASE, PATCH_LEVEL, PATCH_STRING, DATE_STRING);
     fflush(stdout);
 
-#ifdef LOCAL_REGEXP
-    fprintf(stderr, "internal regex\n");
+#define SHOW_RANDOM "random-funcs:"
+#if defined(NAME_SRAND) && defined(NAME_RAND)
+    fprintf(stderr, FMT_S, SHOW_RANDOM, NAME_SRAND "/" NAME_RAND);
 #else
-    fprintf(stderr, "external regex\n");
+    fprintf(stderr, FMT_S, SHOW_RANDOM, "internal");
 #endif
+
+#define SHOW_REGEXP "regex-funcs:"
+#ifdef LOCAL_REGEXP
+    fprintf(stderr, FMT_S, SHOW_REGEXP, "internal");
+#else
+    fprintf(stderr, FMT_S, SHOW_REGEXP, "external");
+#endif
+
     fprintf(stderr, "compiled limits:\n");
-    fprintf(stderr, fmt, "sprintf buffer", (long) SPRINTF_LIMIT);
+    fprintf(stderr, FMT_N, "sprintf buffer", (double) SPRINTF_LIMIT);
+    fprintf(stderr, FMT_N, "maximum-integer", (double) MAX__INT);
+#if 0
+    /* we could show these, but for less benefit: */
+    fprintf(stderr, FMT_N, "maximum-unsigned", (double) MAX__UINT);
+    fprintf(stderr, FMT_N, "maximum-long", (double) MAX__LONG);
+#endif
     mawk_exit(0);
 }
